e24dd47cb5fd9e33c09fc2104406ea3625a8c788
Ryo Matsukawa <76232929+ryo-manba@users.noreply.github.com>
2025-09-12 23:18:20 +0900

    using defineConfig

--- eslint/simple-rules/eslint.config.js
+++ eslint/simple-rules/eslint.config.js
@@ -1,15 +1,14 @@
+import { defineConfig } from "eslint/config";
 import css from "@eslint/css";
 
-export default [
+export default defineConfig([
   {
     files: ["**/*.css"],
     language: "css/css",
-    plugins: {
-      css
-    },
+    plugins: { css },
     rules: {
       "css/no-empty-blocks": "error",
       "css/no-duplicate-imports": "error"
     }
   }
-];
+]);
